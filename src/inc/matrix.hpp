790b4977e8f08b895cbede76f0b60a515461ca79
noise2 <b.g.dariush@gmail.com>
2016-10-26 22:51:00 +0330

    @ inc/matrix.hpp: improved

--- src/inc/matrix.hpp
+++ src/inc/matrix.hpp
@@ -32,43 +32,43 @@ public:
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator=(const S& x)
-    { std::for_each(this->data(), this->data() + this->num_elements(), [&x](auto& i) { i = x; }); return *this; }
+    { this->for_each([&x](auto& i) { i = x; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator=(const std::initializer_list<S> x)
-    { size_t e = 0; std::all_of(x.begin(), x.end(), [&e, this](const auto& i) { if(e < this->num_elements()) { this->data()[e++] = i; return true; } return false; }); return *this; }
+    { size_t e = 0; std::all_of(x.begin(), x.end(), [&e, this](const auto& i) { if(e < this->num_elements()) { this->data()[e++] = i; } return false; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator+=(const S& x)
-    { std::for_each(this->data(), this->data() + this->num_elements(), [&x](auto& i) { i += x; }); return *this; }
+    { this->for_each([&x](auto& i) { i += x; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator-=(const S& x)
-    { std::for_each(this->data(), this->data() + this->num_elements(), [&x](auto& i) { i -= x; }); return *this; }
+    { this->for_each([&x](auto& i) { i -= x; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator*=(const S& x)
-    { std::for_each(this->data(), this->data() + this->num_elements(), [&x](auto& i) { i *= x; }); return *this; }
+    { this->for_each([&x](auto& i) { i *= x; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator/=(const S& x)
-    { std::for_each(this->data(), this->data() + this->num_elements(), [&x](auto& i) { i /= x; }); return *this; }
+    { this->for_each([&x](auto& i) { i /= x; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator+=(const matrix<S, N>& x)
-    { size_t e = 0; std::for_each(this->data(), this->data() + this->num_elements(), [&e, &x](auto& i) { i += x.data()[e++]; }); return *this; }
+    { size_t e = 0; this->for_each([&e, &x](auto& i) { i += x.data()[e++]; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator-=(const matrix<S, N>& x)
-    { size_t e = 0; std::for_each(this->data(), this->data() + this->num_elements(), [&e, &x](auto& i) { i -= x.data()[e++]; }); return *this; }
+    { size_t e = 0; this->for_each([&e, &x](auto& i) { i -= x.data()[e++]; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator*=(const matrix<S, N>& x)
-    { size_t e = 0; std::for_each(this->data(), this->data() + this->num_elements(), [&e, &x](auto& i) { i *= x.data()[e++]; }); return *this; }
+    { size_t e = 0; this->for_each([&e, &x](auto& i) { i *= x.data()[e++]; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix& operator/=(const matrix<S, N>& x)
-    { size_t e = 0; std::for_each(this->data(), this->data() + this->num_elements(), [&e, &x](auto& i) { i /= x.data()[e++]; }); return *this; }
+    { size_t e = 0; this->for_each([&e, &x](auto& i) { i /= x.data()[e++]; }); return *this; }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
     inline matrix operator+(const matrix<S, N>& x) const
@@ -102,33 +102,93 @@ public:
     inline matrix operator/(const S& x) const
     { auto o = *this; o /= x; return o; }
 
-    inline T min() const
-    { T x = *this->data(); std::for_each(this->data(), this->data() + this->num_elements(), [&x](const auto& i) { if(i < x) x = i; }); return x; }
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator==(const S& x) const
+    { size_t e = 0; this->for_each([&x, &e](const auto& i) { if(i == static_cast<T>(x) && ++e) return true; return false; }); return e == this->num_elements(); }
 
-    inline T max() const
-    { T x = *this->data(); std::for_each(this->data(), this->data() + this->num_elements(), [&x](const auto& i) { if(i > x) x = i; }); return x; }
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator!=(const S& x) const
+    { return !this->operator ==(x); }
 
-    inline matrix<T, N>& abs() const
-    { std::for_each(this->data(), this->data() + this->num_elements(), [](const auto& i) { i = ((i < 0) ? i = i * -1 : i); }); return *this; }
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator<(const S& x) const
+    { size_t e = 0; this->all_of([&x, &e](const auto& i) { if(i < static_cast<T>(x) && ++e) return true; return false; }); return e == this->num_elements(); }
 
-    inline T norm2() const
-    { T x = T(); std::for_each(this->data(), this->data() + this->num_elements(), [&x](const auto& i) { x += i * i; }); return x; }
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator>(const S& x) const
+    { size_t e = 0; this->all_of([&x, &e](const auto& i) { if(i > static_cast<T>(x) && ++e) return true; return false; }); return e == this->num_elements(); }
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator<=(const S& x) const
+    { return !this->operator >(x); }
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator>=(const S& x) const
+    { return !this->operator <(x); }
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator==(const matrix<S, N>& x) const
+    { size_t e = 0; this->all_of([&x, &e](const auto& i) { if(i == static_cast<T>(x.data()[e]) && ++e) return true; return false; }); return e == this->num_elements();}
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator!=(const matrix<S, N>& x) const
+    { return !this->operator ==(x); }
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator<(const matrix<S, N>& x) const
+    { size_t e = 0; this->all_of([&x, &e](const auto& i) { if(i < static_cast<T>(x.data()[e]) && ++e) return true; return false; }); return e == this->num_elements();}
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator>(const matrix<S, N>& x) const
+    { size_t e = 0; this->all_of([&x, &e](const auto& i) { if(i > static_cast<T>(x.data()[e]) && ++e) return true; return false; }); return e == this->num_elements();}
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator<=(const matrix<S, N>& x) const
+    { return !this->operator >(x); }
+
+    template<typename S, typename = typename std::enable_if<std::is_convertible<S, T>::value>::type>
+    inline bool operator>=(const matrix<S, N>& x) const
+    { return !this->operator <(x); }
 
     inline matrix<double, N> normalize() const
     { return this->as<double>() / std::accumulate(this->begin(), this->end(), 0.0); }
 
     template<typename S, typename = typename std::enable_if<std::is_convertible<T, S>::value>::type>
     inline matrix<S, N> as() const
-    { size_t e = 0; matrix<S, N> out(this->_sizes); std::for_each(this->data(), this->data() + this->num_elements(), [&out, &e](const auto& i) { out.data()[e++] = static_cast<S>(i); }); return out; }
+    { size_t e = 0; matrix<S, N> out(this->_sizes); this->for_each([&out, &e](const auto& i) { out.data()[e++] = static_cast<S>(i); }); return out; }
 
     friend std::ostream& operator << (std::ostream& os, const matrix<T, N>& m)
-    { std::for_each(m.data(), m.data() + m.num_elements(), [&os](const auto& i) { os << i << " "; }); return os; }
+    { m.for_each([&os](const auto& i) { os << i << " "; }); return os; }
 
     inline typename base::element& operator()(const std::array<size_t, N>& l)
     { return base::operator ()(l); }
 
     inline typename base::element operator()(const std::array<size_t, N>& l) const
     { return base::operator ()(l); }
+
+    template<typename callback>
+    inline void for_each(callback c) { for(size_t i = 0; i < this->num_elements(); i++) c(this->data()[i]); }
+
+    template<typename callback>
+    inline void for_each(callback c) const { for(size_t i = 0; i < this->num_elements(); i++) c(this->data()[i]); }
+
+    template<typename callback>
+    inline void all_of(callback c) { for(size_t i = 0; i < this->num_elements(); i++) if(!c(this->data()[i])) break; }
+
+    template<typename callback>
+    inline void all_of(callback c) const { for(size_t i = 0; i < this->num_elements(); i++) if(!c(this->data()[i])) break; }
+
+    inline T min() const
+    { T x = *this->data(); this->for_each([&x](const auto& i) { if(i < x) x = i; }); return x; }
+
+    inline T max() const
+    { T x = *this->data(); this->for_each([&x](const auto& i) { if(i > x) x = i; }); return x; }
+
+    inline matrix<T, N> abs() const
+    { matrix<T, N> out = *this; out.for_each([](auto& i) { i = ((i < 0) ? i * -1 : i); }); return out; }
+
+    inline T norm2() const
+    { T x = T(); this->for_each([&x](const auto& i) { x += i * i; }); return x; }
 
     template<size_t S>
     matrix<T, N - S> slice(const std::array<size_t, S>& indices) const {
@@ -141,9 +201,10 @@ public:
             BOOST_ASSERT_MSG(indices[i] < this->shape()[i], "index overflow!");
             start += indices[i] * this->strides()[i];
         }
-        std::for_each(out.data(), out.data() + out.num_elements(), [&](auto& i) { i = this->data()[start++]; });
+        out.for_each([&](auto& i) { i = this->data()[start++]; });
         return out;
     }
+
     template<size_t S>
     matrix<T*, N - S> slice_ref(const std::array<size_t, S>& indices) {
         size_t start = 0;
@@ -155,7 +216,7 @@ public:
             BOOST_ASSERT_MSG(indices[i] < this->shape()[i], "index overflow!");
             start += indices[i] * this->strides()[i];
         }
-        std::for_each(out.data(), out.data() + out.num_elements(), [&](auto& i) { i = &(this->data()[start++]); });
+        out.for_each([&](auto& i) { i = &(this->data()[start++]); });
         return out;
     }
 };
