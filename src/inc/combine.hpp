fcc3283340a0e5b756452622c815ee3b6e3f4cdf
noise2 <b.g.dariush@gmail.com>
2016-10-27 12:11:46 +0330

    @ inc/combine.hpp: SEP matrix combination method improved

--- src/inc/combine.hpp
+++ src/inc/combine.hpp
@@ -21,11 +21,10 @@ public:
     }
     template<size_t state_dim, size_t action_dim>
     void sep_combiner(vector<agent<state_dim, action_dim>>& agents, const string& = "") {
-        matrix<scalar, state_dim + action_dim> combined_sep(agents.front().learner->Q.size());
-
         vector<matrix<scalar, state_dim + action_dim>*> Qs;
         vector<matrix<scalar, state_dim + action_dim>*> seps;
         vector<matrix<size_t, state_dim + action_dim>*> shocks;
+        matrix<scalar, state_dim + action_dim> combined_sep(agents.front().template get_plugin<plugin_SEP>()->sep.size());
         for(size_t i = 0; i < agents.size(); i++) {
             auto sepp = agents[i].template get_plugin<plugin_SEP>();
             seps.push_back(&sepp->sep);
@@ -35,14 +34,11 @@ public:
         // combine seps by apply min
         size_t e = 0;
         std::for_each(combined_sep.data(), combined_sep.data() + combined_sep.num_elements(), [&](auto& i) {
-            scalar _min = numeric_limits<scalar>::max();
+            scalar _min = numeric_limits<scalar>::infinity();
             for(size_t i = 0; i < agents.size(); i++)
-                if(seps[i]->data()[e] < _min)
-                    _min = seps[i]->data()[e];
-            i = _min;
-            e++;
+                _min = min(_min, seps[i]->data()[e]);
+            i = _min, e++;
         });
-
     }
     template<size_t state_dim, size_t action_dim>
     void fci_combiner(vector<agent<state_dim, action_dim>>&, const string& = "")  {
