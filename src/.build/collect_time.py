1a9ef6744f203d6fedcbf1e41abc25063351d542
noise2 <b.g.dariush@gmail.com>
2016-10-28 23:37:02 +0330

    + data/maze/exec.kmean-sep-il.time.mat

--- src/.build/collect_time.py
+++ src/.build/collect_time.py
@@ -17,11 +17,12 @@ times = np.zeros((3, max_trials))
 for idx, config in enumerate(configs):
 	for trials in xrange(0, max_trials):
 		command = "cd %s && make -j8 -l8 && ./thesis --agent 3 --iters %i --trials %i %s &>/dev/null" %(os.path.dirname(os.path.realpath(__file__)), iters, trials, config[0]);
-		print "executing `\033[33m%s\033[m`" %command
+		print "executing `\033[33m%s\033[m`" %command,
 		start_time = time.time()
 		proc = subprocess.Popen([command, ""], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 		(out, err) = proc.communicate()
-		times[idx, trials] = (time.time() - start_time) / iters
+		times[idx, trials] = ((time.time() - start_time) * 1e+3) / iters
+		print "%.2f ms" %times[idx, trials]
 
 for i in xrange(0, 3):
 	for j in xrange(0, max_trials):
