5c9b243d7f2c2c3620f210b1b8c7da7efcb1db75
noise2 <b.g.dariush@gmail.com>
2016-11-01 11:41:49 +0330

    @ .build/collect_data.py: solid changes and code layout

--- src/.build/collect_data.py
+++ src/.build/collect_data.py
@@ -1,40 +1,68 @@
 #!/usr/bin/env python2.7
-import os
+import itertools, os;
 
 iters = 20;
-env = "prey";
-trials = 200;
 
-# collecting with different configs
-configs = [
-	['--env %s --method fci --fci-method max --refmat-grind 1' %env, '../data/%s/fci_max_1' %env],
-	['--env %s --method fci --fci-method max --refmat-grind 2' %env, '../data/%s/fci_max_2' %env],
-	['--env %s --method fci --fci-method max --refmat-grind 3' %env, '../data/%s/fci_max_3' %env],
-	['--env %s --method fci --fci-method max --refmat-grind 4' %env, '../data/%s/fci_max_4' %env],
-	['--env %s --method fci --fci-method max --refmat-grind 5' %env, '../data/%s/fci_max_5' %env],
-	['--env %s --method fci --fci-method max --refmat-grind 6' %env, '../data/%s/fci_max_6' %env],
+configs = {
+    "--method" : {
+        "refmat" : {
+            "--env" : {
+                "prey" : {
+                    "--agents" : [3],
+                    "--refmat-grind" : range(1, 18),
+                    "--refmat-combinator" : ["fci-k-mean", "fci-mean", "fci-max", "wsum"],
+                },
+                "maze" : {
+                    "--agents" : [3],
+                    "--refmat-grind" : range(1, 7),
+                    "--refmat-combinator" : ["fci-k-mean", "fci-mean", "fci-max", "wsum"],
+                }
+            }
+        },
+        "sep" : {
+            "--env" : {
+                "prey" : {
+                    "--agents" : [3]
+                },
+                "prey" : {
+                    "--agents" : [3]
+                }
+            }
+        },
+        "il" : {
+            "--env" : {
+                "prey" : {
+                    "--agents" : [1]
+                },
+                "prey" : {
+                    "--agents" : [1]
+                }
+            }
+        }
+    }
+};
 
-	['--env %s --method fci --fci-method mean --refmat-grind 1' %env, '../data/%s/fci_mean_1' %env],
-	['--env %s --method fci --fci-method mean --refmat-grind 2' %env, '../data/%s/fci_mean_2' %env],
-	['--env %s --method fci --fci-method mean --refmat-grind 3' %env, '../data/%s/fci_mean_3' %env],
-	['--env %s --method fci --fci-method mean --refmat-grind 4' %env, '../data/%s/fci_mean_4' %env],
-	['--env %s --method fci --fci-method mean --refmat-grind 5' %env, '../data/%s/fci_mean_5' %env],
-	['--env %s --method fci --fci-method mean --refmat-grind 6' %env, '../data/%s/fci_mean_6' %env],
+def build_commands(configs):
+    commands = [];
+    for method in configs["--method"]:
+        for env in configs["--method"][method]["--env"]:
+            opt = [configs["--method"][method]["--env"][env][i] for i in configs["--method"][method]["--env"][env].keys()]
+            for c in itertools.product(*opt):
+                command = []
+                l = [("--iters", iters), ("--env", env), ("--method", method)];
+                l.extend(zip(configs["--method"][method]["--env"][env].keys(), c))
+                for t in l: command.extend([str(tt) for tt in t]);
+                commands.append((" ".join(command), "method/%s/env/%s" %(method, env)))
+    return commands
 
-	['--env %s --method fci --fci-method k-mean --refmat-grind 1' %env, '../data/%s/fci_k-mean_1' %env],
-	['--env %s --method fci --fci-method k-mean --refmat-grind 2' %env, '../data/%s/fci_k-mean_2' %env],
-	['--env %s --method fci --fci-method k-mean --refmat-grind 3' %env, '../data/%s/fci_k-mean_3' %env],
-	['--env %s --method fci --fci-method k-mean --refmat-grind 4' %env, '../data/%s/fci_k-mean_4' %env],
-	['--env %s --method fci --fci-method k-mean --refmat-grind 5' %env, '../data/%s/fci_k-mean_5' %env],
-	['--env %s --method fci --fci-method k-mean --refmat-grind 6' %env, '../data/%s/fci_k-mean_6' %env],
-
-	['--env %s --method sep' %env, '../data/%s/sep' %env],
-	['--env %s --method il' %env, '../data/%s/il' %env],
-];
-
-
-for config in configs:
-	command = "make -j8 -l8 && ./thesis --agent 3 --iters %i --trials %i %s 1>%s.dat" %(iters, trials, config[0], config[1]);
-	print "executing `\033[33m%s\033[m`" %command
-	os.system(command);
-	print "\n------\n"
+for c in build_commands(configs):
+    redir_dir  = "../data/%s" %c[1]
+    redir_file = "'%s/%s.mat'" %(redir_dir, c[0])
+    exe = "make -j8 -l8 && ./thesis %s" %(c[0]);
+    print "executing: `\033[33m%s\033[m`" %exe
+    print "output file: `\033[33m%s\033[m`" %redir_file
+    if(not os.path.isdir(redir_dir)):
+        os.system("mkdir -p %s" %redir_dir)
+    command = exe + " 1>%s" %redir_file
+    os.system(command);
+    print "\n------\n"
