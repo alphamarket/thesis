140f7f709f3b2c8be40d795f2c3e0e27ba837aa6
noise2 <b.g.dariush@gmail.com>
2016-11-17 09:42:42 +0330

    + .build/collect_data_{1,5}.py

--- src/.build/collect_data.py
+++ src/.build/collect_data.py
@@ -1,79 +1,8 @@
 #!/usr/bin/env python2.7
-import itertools, os;
+import time, os
 
-iters = 20;
-
-rl_action_advisor = "greedy"
-
-configs = {
-    "--method" : {
-        "refmat" : {
-            "--env" : {
-                "prey" : {
-                    "--agents" : [3],
-                    "--refmat-grind" : range(1, 18),
-                    "--refmat-combinator" : ["fci-k-mean", "fci-mean", "fci-max", "wsum", "fci-const-one"],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                },
-                "maze" : {
-                    "--agents" : [3],
-                    "--refmat-grind" : range(1, 7),
-                    "--refmat-combinator" : ["fci-k-mean", "fci-mean", "fci-max", "wsum", "fci-const-one"],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                }
-            }
-        },
-        "sep" : {
-            "--env" : {
-                "prey" : {
-                    "--agents" : [3],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                },
-                "maze" : {
-                    "--agents" : [3],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                }
-            }
-        },
-        "il" : {
-            "--env" : {
-                "prey" : {
-                    "--agents" : [1],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                },
-                "maze" : {
-                    "--agents" : [1],
-                    "--rl-action-advisor" : [rl_action_advisor]
-                }
-            }
-        }
-    }
-};
-
-def build_commands(configs):
-    commands = [];
-    for method in configs["--method"]:
-        for env in configs["--method"][method]["--env"]:
-            opt = [configs["--method"][method]["--env"][env][i] for i in configs["--method"][method]["--env"][env].keys()]
-            for c in itertools.product(*opt):
-                command = []
-                l = [("--iters", iters), ("--env", env), ("--method", method)];
-                l.extend(zip(configs["--method"][method]["--env"][env].keys(), c))
-                for t in l: command.extend([str(tt) for tt in t]);
-                commands.append((" ".join(command), "method/%s/env/%s" %(method, env)))
-    return commands
-
-commands = build_commands(configs);
-
-
-for idx, c in enumerate(commands):
-    redir_dir  = "../data/%s" %c[1]
-    redir_file = "'%s/%s.mat'" %(redir_dir, c[0])
-    exe = "make -j8 -l8 && ./thesis %s" %(c[0]);
-    print "[%.2f%%] executing: `\033[33m%s\033[m`" %((idx + 1) * 100. / len(commands), exe)
-    print "output file: `\033[33m%s\033[m`" %redir_file
-    if(not os.path.isdir(redir_dir)):
-        os.system("mkdir -p %s" %redir_dir)
-    command = exe + " 1>%s" %redir_file
-    os.system(command);
-    print "\n------\n"
+pwd = os.path.dirname(os.path.realpath(__file__))
+for i in range(1, 6):
+    os.system('nohup %s/collect_data_%i.py &>%s/collect_data_%i.log&' %(pwd, i, pwd, i));
+    print "Executing `./collect_data_%i.py` logging into collect_data_%i.log." %(i, i)
+    time.sleep(1);
---
+++ src/.build/collect_data_4.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2.7
+import itertools, os;
+
+iters = 20;
+
+rl_action_advisor = "greedy"
+
+configs = {
+    "--method" : {
+        "sep" : {
+            "--env" : {
+                "prey" : {
+                    "--agents" : [3],
+                    "--rl-action-advisor" : [rl_action_advisor]
+                },
+                "maze" : {
+                    "--agents" : [3],
+                    "--rl-action-advisor" : [rl_action_advisor]
+                }
+            }
+        }
+    }
+};
+
+def build_commands(configs):
+    commands = [];
+    for method in configs["--method"]:
+        for env in configs["--method"][method]["--env"]:
+            opt = [configs["--method"][method]["--env"][env][i] for i in configs["--method"][method]["--env"][env].keys()]
+            for c in itertools.product(*opt):
+                command = []
+                l = [("--iters", iters), ("--env", env), ("--method", method)];
+                l.extend(zip(configs["--method"][method]["--env"][env].keys(), c))
+                for t in l: command.extend([str(tt) for tt in t]);
+                commands.append((" ".join(command), "method/%s/env/%s" %(method, env)))
+    return commands
+
+commands = build_commands(configs);
+
+
+for idx, c in enumerate(commands):
+    redir_dir  = "../data/%s" %c[1]
+    redir_file = "'%s/%s.mat'" %(redir_dir, c[0])
+    exe = "make -j8 -l8 && ./thesis %s" %(c[0]);
+    print "[%.2f%%] executing: `\033[33m%s\033[m`" %((idx + 1) * 100. / len(commands), exe)
+    print "output file: `\033[33m%s\033[m`" %redir_file
+    if(not os.path.isdir(redir_dir)):
+        os.system("mkdir -p %s" %redir_dir)
+    command = exe + " 1>%s" %redir_file
+    os.system(command);
+    print "\n------\n"
